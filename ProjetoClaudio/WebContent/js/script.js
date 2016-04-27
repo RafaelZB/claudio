198a6faeabe9a1a9210621e6a17d5c1295290670
Rafael <Rafael@192.168.1.2>
2016-04-27 17:26:33 -0300

    Correção sufix/prefix moeda

--- ProjetoClaudio/WebContent/js/script.js
+++ ProjetoClaudio/WebContent/js/script.js
@@ -222,7 +222,7 @@
 			negativo = true;
 		c = isNaN(c = Math.abs(c)) ? 2 : c, d = d == undefined ? "," : d, t = t == undefined ? "." : t, s = n < 0 ? "-" : "", i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "", j = (j = i.length) > 3 ? j % 3 : 0;
 		xyz =  s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
-		xyz = xyz + siglaMoeda;
+		xyz = siglaMoeda + xyz;
 		if(negativo)
 			xyz = "<span class='red'>" + xyz + "</span>";
 		return xyz;
